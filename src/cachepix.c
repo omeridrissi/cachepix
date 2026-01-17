46ca828d59ec932d4073ca0b75b15ad4e4f6f77b
omer <omer.e.idrissi@gmail.com>
2026-01-17 12:42:56 +0100

    Fix compilation errors

--- src/cachepix.c
+++ src/cachepix.c
@@ -398,39 +398,5 @@ int ppm_copy(PPM_ptr dst_ptr, const PPM_ptr src_ptr) {
     return 0;
 }
 
-int ppm_convert_maxval(PPM_ptr img_ptr, uint16_t new_maxval) {
-    uint16_t old_maxval = img_ptr->maxval;
-    
-    uint32_t bytes_per_channel = 1;
-    if (old_maxval > 255) 
-        bytes_per_channel = 2;
-
-    if (old_maxval =< 255 && new_maxval > 255) {
-        return -1;  // Doesn't allow a change in maxval that affects the number of bytes per channel
-    } else if (old_maxval >= 256 && new_maxval < 256) {
-        return -1;
-    } else {
-
-        if (bytes_per_channel = 2) {
-            for (int i = 0; i < img_ptr->data_size; i+=2) {
-                double intensity = ((uint16_t*)(img_ptr->data + i))/old_maxval;
-                uint16_t new_val = (uint16_t)(intensity*new_maxval);
-                img_ptr->data[i] = new_val;
-            } 
-        } else {
-            for (int i = 0; i < img_ptr->data_size; i++) {
-                double intensity = img_ptr->data[i]/old_maxval;
-                uint16_t new_val = (uint16_t)(intensity*new_maxval);
-                img_ptr->data[i] = new_val;
-            }
-        }
-        
-
-        img_ptr->maxval = new_maxval;
-    }
-    return 0;
-}
-
-
 
 
--- src/scalar.c
+++ src/scalar.c
@@ -7,15 +7,15 @@ int ppm_convert_maxval(PPM_ptr img_ptr, uint16_t new_maxval) {
     if (old_maxval > 255) 
         bytes_per_channel = 2;
 
-    if (old_maxval =< 255 && new_maxval > 255) {
+    if (old_maxval <= 255 && new_maxval > 255) {
         return -1;  // Doesn't allow a change in maxval that affects the number of bytes per channel
     } else if (old_maxval >= 256 && new_maxval < 256) {
         return -1;
     } else {
 
-        if (bytes_per_channel = 2) {
+        if (bytes_per_channel == 2) {
             for (int i = 0; i < img_ptr->data_size; i+=2) {
-                double intensity = ((uint16_t*)(img_ptr->data + i))/old_maxval;
+                double intensity = (*(uint16_t*)(img_ptr->data + i))/old_maxval;
                 uint16_t new_val = (uint16_t)(intensity*new_maxval);
                 img_ptr->data[i] = new_val;
             } 
@@ -25,10 +25,12 @@ int ppm_convert_maxval(PPM_ptr img_ptr, uint16_t new_maxval) {
                 uint16_t new_val = (uint16_t)(intensity*new_maxval);
                 img_ptr->data[i] = new_val;
             }
-        }
+        }        
+
         img_ptr->maxval = new_maxval;
     }
     return 0;
 }
+
 
 
